10d259ae05f8c5ff7b801e1f99221f8cba7bec9d
Kuldeep Singh  Argal <kuldeep.argal@ad.infosys.com>
2023-11-14 00:18:33 +0530

    initial commit

---
+++ src/main/resources/db/migration/V1_1_0__my_first_migration.sql
@@ -0,0 +1,8 @@
+	CREATE TABLE IF NOT EXISTS employee (
+
+	    id  serial,
+	    name varchar(20),
+	    email varchar(50),
+	    date_of_birth timestamp
+
+	);
---
+++ src/main/resources/db/migration/V2_1_0__my_first_migration.sql
@@ -0,0 +1,1 @@
+alter table employee add column alternate_email varchar(100);
